360bd33e6fdf39af85de302a33085315a7a4fb3e
sallesalex01 <alex.salles2007@gmail.com>
2023-08-31 10:53:24 -0300

    Listar Revistas

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Livro[] livros = new Livro[1000];
         Revista[] revistas = new Revista[1000];
-        int escolha = 0, NumCadastroLivros = 0, NumCadastroRevistas = 0;
+        int escolha, NumCadastroLivros = 0, NumCadastroRevistas = 0;
         String nomeLivro, autor, editora, prateleira, ISBN, CDD, edicao, ano, secao, issn;
         do {
             System.out.println("\n=======================================");
@@ -15,11 +15,12 @@ public class Main {
             System.out.println("[1] - Cadastrar livro"); // de 0 a 999
             System.out.println("[2] - Cadastrar Revista"); // de 0 a 999
             System.out.println("[3] - Procurar livro"); // por palavra chave
-            System.out.println("[4] - Listar todos livros");
-            System.out.println("[5] - Quantidade cadastrada/livre"); // x livros cadastrados | y livre
-            System.out.println("[6] - Excluir último elemento da lista");
-            System.out.println("[7] - Ordenar livros cadastrados");
-            System.out.println("[8] - Sair");
+            System.out.println("[4] - Listar todos os livros");
+            System.out.println("[5] - Listar todos as Revistas");
+            System.out.println("[6] - Quantidade cadastrada/livre"); // x livros cadastrados | y livre
+            System.out.println("[7] - Excluir último elemento da lista");
+            System.out.println("[8] - Ordenar livros cadastrados");
+            System.out.println("[9] - Sair");
             System.out.println("\n=======================================\n");
             System.out.print("Escolha uma das opçoes: ");
             escolha = input.nextInt();
@@ -145,9 +146,27 @@ public class Main {
                         }
                     }
                     break;
+                case 5:
+                    if (NumCadastroRevistas == 0) {
+                        System.out.println("Nenhum livro cadastrado ainda.");
+                    } else {
+                        for (int i = 0; i < NumCadastroRevistas; i++) {
+                            System.out.println("Revistas: " + (i +1));
+                            System.out.println(revistas[i].getNomeLivro() + ", " +
+                                    revistas[i].getAutor()+", " +
+                                    revistas[i].getISBN() + ", "+
+                                    revistas[i].getAno()+ ", "+
+                                    revistas[i].getEditora()+ ", "+
+                                    revistas[i].getPrateleira()+ ", "+
+                                    revistas[i].getSecao()+ ", "+
+                                    revistas[i].getCDD()+ ", "+
+                                    revistas[i].getEdicao());
+                        }
+                    }
+                    break;
 
                 /* Quantidade Cadastrada/livre */
-                case 5:
+                case 6:
                     System.out.println("\n=================================");
                     System.out.printf("%-30s %-1s", "| Quantidade cadastrada: " + NumCadastroLivros, " |");
                     System.out.println();
@@ -157,7 +176,7 @@ public class Main {
                     break;
 
                 /* Excluir ultimo livro da lista */
-                case 6:
+                case 7:
                    if (NumCadastroLivros > 0 ) {
                        NumCadastroLivros--;
                        System.out.println("Numero de livros: " + NumCadastroLivros);
@@ -168,13 +187,13 @@ public class Main {
                     break;
 
                 /* Ordenar livros cadastrados */
-                case 7:
+                case 8:
 
                     Arrays.sort(livros);
                     break;
             }
 
-        } while (escolha != 8);
+        } while (escolha != 9);
 
         input.close();
     }
